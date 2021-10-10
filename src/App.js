1bccf4846335235b4aedbcc2d2a423fcdd3d7496
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-09 20:04:52 -0700

    Add text emphasis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<h1>Cullen Sharp</h1>
 			<section className='bodyCopy'>
 				<p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<mark>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
 				</p>
 				<nav>
 					<ul>
