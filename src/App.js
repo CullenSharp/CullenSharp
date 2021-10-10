c8e0993cb04699b2e99951825ea88d6c4f278741
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-09 19:55:57 -0700

    HTML and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<h1>Cullen Sharp</h1>
+			<section className='bodyCopy'>
+				<p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<nav>
+					<ul>
+						<li>
+							<a href="https://github.com/CullenSharp">github,</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/cullensharp/">linkedin,</a>
+						</li>
+						<li>
+							<a href="https://www.are.na/cullen-sharp">are.na</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+		</div>
+	);
 }
 
 export default App;
