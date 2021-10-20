fd819b764d2ce87e950866032575b4e6312e93d6
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-19 22:31:34 -0700

    Add environment variables to Sanity client

--- src/client.js
+++ src/client.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
 	projectId: 'cq5ohcjc',
 	dataset: 'production',
+	apiVersion: '2021-10-20',
+	token: process.env.REACT_APP_SANITY_TOKEN,
 	useCdn: true
 });
