890e764037cc206918315afd3c691c6d0805e498
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-25 14:36:52 -0700

    Add tighter tracking to title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	mode: 'jit',
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
 			color: {
