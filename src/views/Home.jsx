abc7450f0d1fb23621a2268f11053dffc5788127
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-25 14:24:35 -0700

    Change styling

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Home() {
 	return(
-		<section>
-			<h1>Cullen Sharp</h1>
-			<p>
-				<mark>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
+		<div className='view'>
+			<h1 className='title'>Cullen Sharp</h1>
+			<p className='intro'>
+				<mark className='highlight'>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
 			</p>
 			<nav>
 				<ul>
@@ -19,14 +19,9 @@ function Home() {
 					<li>
 						<a href="https://www.are.na/cullen-sharp">are.na,</a>
 					</li>
-					<li>
-						<Link to='/Writings'>
-                writing
-						</Link>
-					</li>
 				</ul>
 			</nav>
-		</section>
+		</ div>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+			color: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				'primary': '#17931A',
+				'mid-grey': '#C9C9C9',
+				'light': '#F4F8EE',
+				'dark': '#011A00',
+				'body-dark': '#FDFDFD',
+				'body-light': '#131313',
+				'hyperlink-dark': '#3ED23B',
+				'hyperlink-light': '#316334',
+			},
 			fontFamily: {
 				'display': ['Sporting Grotesque Bold'],
 				'body': ['IBM Plex Sans Light']
