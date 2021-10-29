2566ea6607e7270bec8b3d4b1d734455f8ed19cc
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-28 21:50:51 -0700

    Fixes #12,#13,#15

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import resume from '../assets/resume.pdf';
-
 
 function Home() {
 	return(
 		<div className='view'>
-			<h1 className='title'>Cullen Sharp</h1>
-			<p className='intro'>
-				<mark className='highlight'>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
+			<h1 className='text-[#131313] dark:text-[#FDFDFD] font-display mt-28 tracking-tighter text-5xl mb-7 lg:mb-14 lg:text-8xl'>Cullen Sharp</h1>
+			<p className='font-body border-b-2 border-[#131313] dark:border-[#FDFDFD] pb-8 mb-8 tracking-wide text-base lg:text-4xl lg:tracking-tight'>
+				<mark>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
 			</p>
 			<nav>
 				<ul className='mb-2'>
-					<li>
-						<Link to='/Writings'>📓 writings</Link>
-					</li>
-					<li>
-						<Link to='/Projects'>💼 projects</Link>
-					</li>
-					<li>
-						<a href={resume}>📝 resume</a>
-					</li>
-				</ul>
-				<ul>
 					<li>
 						<a href="https://github.com/CullenSharp" target="_blank" rel="noopener noreferrer">💻 github↗</a>
 					</li>
@@ -31,6 +18,9 @@ function Home() {
 					</li>
 					<li>
 						<a href="https://www.are.na/cullen-sharp" target="_blank" rel="noopener noreferrer">*️⃣ are.na↗</a>
+					</li>
+					<li>
+						<a href={resume}>📝 resume</a>
 					</li>
 				</ul>
 			</nav>
