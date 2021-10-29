fa01b094cd8bff8a2f7b43ef5d60f3c1dea8cebf
Cullen Sharp <cullen.sharp100@gmail.com>
2021-10-29 10:43:19 -0700

    Change margin on header

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ import resume from '../assets/resume.pdf';
 function Home() {
 	return(
 		<div className='view'>
-			<h1 className='text-[#131313] dark:text-[#FDFDFD] font-display mt-28 tracking-tighter text-5xl mb-7 lg:mb-14 lg:text-8xl'>Cullen Sharp</h1>
+			<h1 className='text-[#131313] dark:text-[#FDFDFD] font-display mt-26 tracking-tighter text-5xl mb-7 lg:mb-14 lg:text-8xl'>Cullen Sharp</h1>
 			<p className='font-body border-b-2 border-[#131313] dark:border-[#FDFDFD] pb-8 mb-8 tracking-wide text-base lg:text-4xl lg:tracking-tight'>
 				<mark>Fullstack Web Developer</mark> with hands-on experience in the design and development of web applications from concept to release. Motivated self-starter focused on working at the intersection of art, design, and technology to create <mark>immersive</mark> and <mark>intuitive</mark> user experiences.
 			</p>
@@ -19,6 +19,8 @@ function Home() {
 					<li>
 						<a href="https://www.are.na/cullen-sharp" target="_blank" rel="noopener noreferrer">*️⃣ are.na↗</a>
 					</li>
+				</ul>
+				<ul className='mb-2'>
 					<li>
 						<a href={resume}>📝 resume</a>
 					</li>
